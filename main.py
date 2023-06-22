9ecf73f9fca982485ed834dc78218c19952afe8a
Replit user <>
2023-06-22 03:22:48 +0000

    Initial Commit

--- main.py
+++ main.py
@@ -2,24 +2,40 @@
 
 # -- Import section --
 from flask import Flask
-# from flask import render_template
+from flask import render_template
 # from flask import request
-
 
 # -- Initialization section --
 app = Flask(__name__)
 
 # -- Routes section --
 
+
 # INDEX
 @app.route('/')
 @app.route('/index')
 def index():
-  return "hello world"
+  props = {
+    'title': 'Home',
+    'first_name': 'Alejandra',
+  }
+
+  return render_template('index.html', props=props)
 
 
+@app.route('/secret')
+def secret():
+  return ("Congrats! You found the secret!")
 
+
+@app.route('/results')
+def results():
+  props = {
+    'name': 'Felix',
+    'favorite_animal': 'wolf',
+  }
+  return (render_template('resultspage.html', props=props))
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+  app.run(host='0.0.0.0', port=5000)
